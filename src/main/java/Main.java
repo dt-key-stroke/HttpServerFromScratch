c6fd3204fb1a7b5fc4e16dca14b2cad50a46388d
Deepak. T <deepak.t@aiseberg.com>
2025-12-31 15:32:40 +0530

    Respond with body

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,37 +1,48 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Main {
-  public static void main(String[] args) {
-    System.out.println("Starting the server...");
 
+  public static void parseRequest(Socket sock) throws IOException {
+    // byte[] buff = new byte[20];
+    BufferedReader br = new BufferedReader(new InputStreamReader(sock.getInputStream()));
+    String firstLine = br.readLine();
+    String[] request_line = firstLine.split(" ");
+    System.out.println("Able to split headers");
+    String url = request_line[1];
+    var param = url.split("/")[2];
+    var response_body = param;
+    var repsonse_length = response_body.length();
+
+    sendResponse(sock, String.format("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %s\r\n\r\n%s", repsonse_length, response_body));
+  }
+
+  public static void sendResponse(Socket sock, String content) throws IOException {
+    sock.getOutputStream().write(content.getBytes());
+  }
+
+  public static void main(String[] args) throws IOException {
+    System.out.println("Starting the server...");
+    ServerSocket serverSocket = null;
     try {
-      ServerSocket serverSocket = new ServerSocket(4221);
+      serverSocket = new ServerSocket(4221);
     
       serverSocket.setReuseAddress(true);
     
-      Socket recv = serverSocket.accept(); // Wait for connection from client.
+      Socket recv = serverSocket.accept();
       System.out.println("Got something...");
-      byte[] buff = new byte[20];
-      recv.getInputStream().read(buff);
-      String request = new String(buff);
-      System.out.println("got a str");
-      String[] split_request = request.split("\r\n");
-      String[] request_line = split_request[0].split(" ");
-      String url = request_line[1];
 
-      System.out.println("inp_str: " + request);
-      System.out.println("req_line: " + request_line);
-      System.out.println("req_split: " + split_request);
-      if(url.equals("/")) {
-        recv.getOutputStream().write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
-      } else {
-        recv.getOutputStream().write("HTTP/1.1 404 Not Found\r\n\r\n".getBytes());
-      }
+      parseRequest(recv);
       System.out.println("Sent the response connection");
     } catch (IOException e) {
       System.out.println("IOException: " + e.getMessage());
+    } finally {
+      if (serverSocket != null && !serverSocket.isClosed()) {
+        serverSocket.close();
+      }
     }
   }
 }
