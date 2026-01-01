04d62b89a80967e7515f38597343a56f59fdbe8a
Deepak. T <deepak.t@aiseberg.com>
2026-01-01 14:00:07 +0530

    codecrafters submit [skip ci]

--- src/main/java/Compression.java
+++ src/main/java/Compression.java
@@ -1,27 +1,45 @@
-import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.math.BigInteger;
 import java.util.zip.GZIPOutputStream;
 
 public class Compression {
     private static final int BUFFER_SIZE = 512;
 
-    public static String gzip(String payload) throws IOException {
-        ByteArrayInputStream is = new ByteArrayInputStream(payload.getBytes());
-        ByteArrayOutputStream os = new ByteArrayOutputStream();
-        try (GZIPOutputStream gzipOs = new GZIPOutputStream(os)) {
-            byte[] buffer = new byte[BUFFER_SIZE];
-            int bytesRead;
-            while ((bytesRead = is.read(buffer)) > -1) {
-                gzipOs.write(buffer, 0, bytesRead);
-            }
-        }
-        String compressed = os.toString();
-        return compressed;
+    public static String toHex(byte[] arg) throws UnsupportedEncodingException {
+        return String.format("%040x", new BigInteger(1, arg));
     }
 
-    public static void test() throws IOException {
-        var res = Compression.gzip("pineapple");
+
+    public static String gzip(String payload) throws IOException {
+        var uncompressedData = payload.getBytes();
+        ByteArrayOutputStream bos = null;
+        GZIPOutputStream gzipOS = null;
+        try {
+            bos = new ByteArrayOutputStream(uncompressedData.length);
+            gzipOS = new GZIPOutputStream(bos);
+            gzipOS.write(uncompressedData);
+            gzipOS.close();
+            return toHex(bos.toByteArray());
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        finally {
+            try {
+                assert gzipOS != null;
+                gzipOS.close();
+                bos.close();
+            }
+            catch (Exception ignored) {
+            }
+        }
+        return "";
+    }
+
+    public static void main() throws IOException {
+        var res = Compression.gzip("paaineapple");
         System.out.println(res);
     }
 
